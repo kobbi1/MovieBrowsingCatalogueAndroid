64691f4e409d706c31467a300f454968c4059766
valurmp <valur@agrdynamics.com>
2025-03-30 21:26:29 +0000

    editprofilepic
    Gallery and Camera

--- app/src/main/java/com/example/moviebrowsingcatalogue/services/ApiService.java
+++ app/src/main/java/com/example/moviebrowsingcatalogue/services/ApiService.java
@@ -12,16 +12,19 @@ import com.example.moviebrowsingcatalogue.core.Watchlist;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Body;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Header;
 import retrofit2.http.Field;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 import okhttp3.ResponseBody;
@@ -69,6 +72,19 @@ public interface ApiService {
     );
 
 
+    @Multipart
+    @POST("/images/upload")
+    Call<ResponseBody> uploadProfilePicture(
+            @Part MultipartBody.Part file
+    );
+
+    @Multipart
+    @POST("/users/update/{id}")
+    Call<AuthResponse> updateUserProfileWithImage(
+            @Path("id") Long userId,
+            @Part("user") User user,
+            @Part MultipartBody.Part image
+    );
 
 
     // User Auth
